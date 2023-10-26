4382ef87646a44a4dd0a8f3128a7fbd2cb182fd3
user <il-05-05@mail.ru>
2023-10-26 21:22:51 +0500

    lab2 1 task

---
+++ lab2.py
@@ -0,0 +1,23 @@
+from math import *
+
+
+def task1():
+    def is_prime(num):
+        if num <= 1:
+            return False
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                return False
+        return True
+
+    def is_positive(num):
+        if num > 0:
+            return True
+        else:
+            return False
+
+    number = float(input("Введите число - "))
+    print("Число является простым -", is_prime(number),", Число является положительным -", is_positive(number))
+
+task1()
+
