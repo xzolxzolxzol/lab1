a551a92a884ac4b40bd4ded4e19903e09e49bc48
user <il-05-05@mail.ru>
2023-10-26 23:35:07 +0500

    lab2

--- lab2.py
+++ lab2.py
@@ -17,11 +17,64 @@ def task1():
             return False
 
     number = float(input("Введите число - "))
-    print("Число является простым -", is_prime(number),", Число является положительным -", is_positive(number))
+    print("Число является простым -", is_prime(number), ", Число является положительным -", is_positive(number))
 
 
 def task2():
+    a = float(input())
+    b = float(input())
+
+    x_list = list(range(a, b))
+
+    if x_list >= 0:
+        f = exp(x_list) + x_list
+    else:
+        f = sin(x_list) + 1
+    print("f(x)= {:.4f}".format(f))
+
     pass
 
-task1()
 
+def task3():
+    def number_in_new_numeral_system(number, base):
+        if number == 0:
+            return '0'
+
+        digits = []
+        while number > 0:
+            # Остаток от деления - это цифра в новой системе счисления
+            remainder = number % base
+            # Добавляем цифру в начало списка
+            digits.insert(0, str(remainder))
+            number = number // base
+
+        return ''.join(digits)
+
+    number = int(input("Введите число в десятичной системе счисления: "))
+    base = int(input("Введите основание новой системы счисления: "))
+
+    result = number_in_new_numeral_system(number, base)
+    print(f"Результат: {result}")
+
+
+def task5():
+    def find_smallest_largest_absolute_value(a, b, c):
+        absolute_values = [a, b, c]
+
+
+        smallest = min(absolute_values)
+        largest = max(absolute_values)
+
+        return smallest, largest
+
+    a = float(input("Введите первое число: "))
+    b = float(input("Введите второе число: "))
+    c = float(input("Введите третье число: "))
+
+    smallest_value, largest_value = find_smallest_largest_absolute_value(a, b, c)
+
+    print(f"Наименьшее абсолютное значение: {smallest_value}")
+    print(f"Наибольшее абсолютное значение: {largest_value}")
+
+
+task5()
